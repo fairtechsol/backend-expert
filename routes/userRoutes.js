ff8c9c0164131fdb549697d0f2190e6535156196
dipak-DEV-3055 <dipak.duple@gmail.com>
2023-12-13 17:55:11 +0530

    password route change

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,8 +12,8 @@ const { isAuthenticate } = require('../middleware/auth');
 
 router.post('/add',validator(CreateUser),createUser);
 router.post('/update',validator(UpdateUser),updateUser);
-router.post('/password',validator(ChangePassword),changePassword);
-router.post('/password/self',isAuthenticate,validator(ChangeSelfPassword),changeSelfPassword);
+router.post('/admin/password',validator(ChangePassword),changePassword);
+router.post('/password',isAuthenticate,validator(ChangeSelfPassword),changeSelfPassword);
 
 module.exports = router;
 //https://3100dev.fairgame.club/fair-game-wallet/getUserBalanceDetails

a02473386d92511e83ff9a720d703b9bc1825d96
dipak-DEV-3055 <dipak.duple@gmail.com>
2023-12-13 17:12:43 +0530

    Update user routes and controllers for synchronization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const validator = require('../middleware/joi.validator')
-const {CreateUser, ChangePassword} = require('../validators/userValidator');
-const {createUser, changePassword} = require('../controllers/userController');
+const {CreateUser, ChangePassword, UpdateUser, ChangeSelfPassword} = require('../validators/userValidator');
+const {createUser, changePassword, updateUser, changeSelfPassword} = require('../controllers/userController');
 
 const { isAuthenticate } = require('../middleware/auth');
 
@@ -11,7 +11,9 @@ const { isAuthenticate } = require('../middleware/auth');
 
 
 router.post('/add',validator(CreateUser),createUser);
-router.post('/changePassword',isAuthenticate,validator(ChangePassword),changePassword);
+router.post('/update',validator(UpdateUser),updateUser);
+router.post('/password',validator(ChangePassword),changePassword);
+router.post('/password/self',isAuthenticate,validator(ChangeSelfPassword),changeSelfPassword);
 
 module.exports = router;
 //https://3100dev.fairgame.club/fair-game-wallet/getUserBalanceDetails

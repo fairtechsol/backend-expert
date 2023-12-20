04c8977c838518622845f29f2da6d6f2c3656fd3
dipak-DEV-3055 <dipak.duple@gmail.com>
2023-12-20 16:00:49 +0530

    update cache function to resolve stringiFy value

--- services/redis/commonfunction.js
+++ services/redis/commonfunction.js
@@ -5,25 +5,29 @@ let expiry = 3600;
 exports.addMatchInCache = async (matchId,data) =>{
     let matchKey =`${matchId}_match`;
     let payload = {
-        id: JSON.stringify(data.id),
-        matchType : JSON.stringify(data.matchType),
-        competitionId : JSON.stringify(data.competitionId),
-        competitionName : JSON.stringify(data.competitionName),
-        title : JSON.stringify(data.title),
-        marketId : JSON.stringify(data.marketId),
-        eventId : JSON.stringify(data.eventId),
-        teamA : JSON.stringify(data.teamA),
-        teamB : JSON.stringify(data.teamB),
-        teamC : JSON.stringify(data.teamC),
-        betFairSessionMaxBet : JSON.stringify(data.betFairSessionMaxBet),
-        betFairSessionMinBet : JSON.stringify(data.betFairSessionMinBet),
-        startAt : JSON.stringify(data.startAt),
-        stopAt : JSON.stringify(data.stopAt),
-        apiSessionActive : JSON.stringify(data.apiSessionActive),
-        manualSessionActive : JSON.stringify(data.manualSessionActive),
+        id: data.id,
+        matchType : data.matchType,
+        competitionId : data.competitionId,
+        competitionName : data.competitionName,
+        title : data.title,
+        marketId : data.marketId,
+        eventId : data.eventId,
+        teamA : data.teamA,
+        teamB : data.teamB,
+        betFairSessionMaxBet : data.betFairSessionMaxBet,
+        betFairSessionMinBet : data.betFairSessionMinBet,
+        startAt : data.startAt,
+        apiSessionActive : data.apiSessionActive,
+        manualSessionActive : data.manualSessionActive,
         matchOdd : JSON.stringify(data.matchOdd),
         marketBookmaker : JSON.stringify(data.marketBookmaker),
         marketTiedMatch : JSON.stringify(data.marketTiedMatch)
+    }
+    if(data.teamC){
+        payload.teamC = data.teamC;
+    }
+    if(data.stopAt){
+        payload.stopAt = stopAt;
     }
     let res =  await internalRedis
     .pipeline()
@@ -38,7 +42,7 @@ exports.updateMatchInCache = async (matchId,data) =>{
     let matchKey =`${matchId}_match`;
     let match = await internalRedis.hgetall(matchKey);
     let payload = {
-        id : JSON.stringify(match.id),
+        id : match.id,
         matchType : data.matchType || match.matchType,
         competitionId : data.competitionId || match.competitionId,
         competitionName : data.competitionName || match.competitionName,
@@ -46,17 +50,21 @@ exports.updateMatchInCache = async (matchId,data) =>{
         marketId : data.marketId || match.marketId,
         eventId : data.eventId || match.eventId,
         teamA : data.teamA || match.teamA,
-        teamB : data.teamB || match.teamB, 
-        teamC : data.teamC || match.teamC,
+        teamB : data.teamB || match.teamB,
         betFairSessionMaxBet : data.betFairSessionMaxBet || match.betFairSessionMaxBet,
         betFairSessionMinBet : data.betFairSessionMinBet || match.betFairSessionMinBet,
         startAt : data.startAt || match.startAt,
-        stopAt : data.stopAt || match.stopAt,
         apiSessionActive : data.apiSessionActive ?? match.apiSessionActive,
         manualSessionActive : data.manualSessionActive ?? match.manualSessionActive,
         matchOdd : JSON.stringify(data.matchOdd) || match.matchOdd,
         marketBookmaker : JSON.stringify(data.marketBookmaker) || match.marketBookmaker,
         marketTiedMatch : JSON.stringify(data.marketTiedMatch) || match.marketTiedMatch
+    }
+    if(data.teamC || match.teamC){
+        payload.teamC = data.teamC || match.teamC;
+    }
+    if(data.stopAt || match.stopAt){
+        payload.stopAt = data.stopAt || match.stopAt;
     }
     return await internalRedis
     .pipeline()
@@ -69,7 +77,7 @@ exports.updateMatchKeyInCache = async(matchId,key,data) =>{
     let matchKey =`${matchId}_match`;
     // let match = await internalRedis.hget(matchKey,key);
     let payload = {
-        [key] : JSON.stringify(data)
+        [key] : data
     }
     return await internalRedis
     .pipeline()
@@ -88,4 +96,9 @@ exports.updateMatchExpiry =async (matchId) =>{
 
     return await internalRedis
     .expire(matchKey, expiry);
+}
+
+exports.getMatchFromCache = async(matchId) =>{
+    let matchKey =`${matchId}_match`;
+    return await internalRedis.hgetall(matchKey);
 }

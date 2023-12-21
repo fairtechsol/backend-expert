d69295547c620cb1f3aeddfd1ef338aa6f87e9da
rohanduple <150104513+rohanduple@users.noreply.github.com>
2023-12-21 18:29:23 +0530

    added sidebar match listing and compition list

--- routes/matchRouter.js
+++ routes/matchRouter.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const validator = require('../middleware/joi.validator');
 
 const { isAuthenticate } = require('../middleware/auth');
-const { createMatch, updateMatch, listMatch, matchDetails, matchActiveInActive } = require('../controllers/matchController');
+const { createMatch, updateMatch, listMatch, matchDetails, matchActiveInActive, getMatchCompetitionsByType, getMatchDatesByCompetitionId } = require('../controllers/matchController');
 const {  updateMatchValidate, addMatchValidate, MatchActiveInactive } = require('../validators/matchValidator');
+const { getMatchByCompetitionIdAndDates } = require('../services/matchService');
 
 
 
@@ -14,6 +15,9 @@ router.post('/add',isAuthenticate,validator(addMatchValidate),createMatch);
 router.post('/update',isAuthenticate,validator(updateMatchValidate),updateMatch);
 router.get('/list',isAuthenticate,listMatch);
 router.get('/:id',isAuthenticate,matchDetails);
+router.get('/competitionList/:type',isAuthenticate,getMatchCompetitionsByType);
+router.get('/competition/dates/:competitionId',isAuthenticate,getMatchDatesByCompetitionId);
+router.get('/competition/getMatch/:competitionId/:date',isAuthenticate,getMatchByCompetitionIdAndDates);
 router.post('/updateActiveStatus',isAuthenticate,validator(MatchActiveInactive),matchActiveInActive);
 
 module.exports = router;

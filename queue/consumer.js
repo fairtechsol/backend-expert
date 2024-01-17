37c6bdacd876b598b6055c38d249c7c127666d50
nain4884 <nain4884@gmail.com>
2024-01-17 12:18:24 +0530

    replace

--- queue/consumer.js
+++ queue/consumer.js
@@ -66,7 +66,7 @@ let calculateRateAmount = async (jobData, userId) => {
         process: `User ID : ${userId} expert`,
       });
       //send Data to socket
-      jobData.myStake = Number(((jobData.stake / 100) * partnership).toFixed(2));
+      jobData.myStake = Number(((jobData.stake / 100) * mPartenerShip).toFixed(2));
       sendMessageToUser(socketData.expertRoomSocket, socketData.MatchBetPlaced, { jobData });
     }
     catch (error) {

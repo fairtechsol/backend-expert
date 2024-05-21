a1b18429f36f11544a63e3c812ec218679b1bc87
GauravDuple <gauravmishra.duple@gmail.com>
2024-05-21 16:15:05 +0530

    removing addRummer

--- services/raceBettingService.js
+++ services/raceBettingService.js
@@ -4,11 +4,6 @@ const runnerSchema = require("../models/matchRunner.entity");
 const RacingBetting = AppDataSource.getRepository(racingBettingSchema);
 const RacingRunner = AppDataSource.getRepository(runnerSchema);
 
-exports.addRunner = async (body) => {
-  let runner = await RacingRunner.save(body);
-  return runner;
-};
-
 exports.insertRunners = async (body) => {
   let runner = await RacingRunner.save(body);
   return runner;

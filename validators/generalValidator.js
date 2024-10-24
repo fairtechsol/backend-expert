4196f1d6839fdcf75f8ca41091f41f95156cdac3
rohanduple <150104513+rohanduple@users.noreply.github.com>
2024-10-24 11:25:48 +0530

    Added banner for mobile and desktop

--- validators/generalValidator.js
+++ validators/generalValidator.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const { bannerType } = require('../config/contants');
 
 const MAX_SIZE_IN_BYTES = 1000 * 1024;
 const MAX_BASE64_LENGTH = Math.ceil((MAX_SIZE_IN_BYTES * 4) / 3);
@@ -15,5 +16,8 @@ module.exports.banner = Joi.object({
     .messages({
         'string.base64': 'The provided string must be in Base64 format.',
         'string.max': `Base64 string must not exceed ${MAX_SIZE_IN_BYTES / 1024} KB.`,
+    }),
+    type: Joi.string().valid(Object.values(bannerType)).required().messages({
+        'any.required': `Banner type is a required field`
     })
 })

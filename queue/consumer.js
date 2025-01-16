8935a33f29096afe1431b1ae8de4f86b8b55fc29
rohanduple <150104513+rohanduple@users.noreply.github.com>
2025-01-16 16:21:46 +0530

    Fix profitLoss calculation to ensure precision by rounding to two decimal places

--- queue/consumer.js
+++ queue/consumer.js
@@ -386,7 +386,7 @@ expertSessionBetDeleteQueue.process(async function (job, done) {
           userDeleteProfitLoss.betData.map((ob, index) => {
             let partnershipData = (ob.profitLoss * partnership) / 100;
             if (ob.odds == parentPLbetPlaced[index].odds) {
-              parentPLbetPlaced[index].profitLoss = parseFloat(parentPLbetPlaced[index].profitLoss) + partnershipData;
+              parentPLbetPlaced[index].profitLoss = parseFloat((parseFloat(parentPLbetPlaced[index].profitLoss) + partnershipData).toFixed(2));
               if (newMaxLossParent < Math.abs(parentPLbetPlaced[index].profitLoss) && parentPLbetPlaced[index].profitLoss < 0) {
                 newMaxLossParent = Math.abs(parentPLbetPlaced[index].profitLoss);
               }

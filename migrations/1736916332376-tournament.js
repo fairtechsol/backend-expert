e4fb47267806ab3931e977566491efc77c4236cd
rohanduple <150104513+rohanduple@users.noreply.github.com>
2025-01-16 13:05:04 +0530

    Remove 'rate' column and add 'backRate' and 'layRate' columns to tournamentRunners table

--- migrations/1736916332376-tournament.js
+++ migrations/1736916332376-tournament.js
@@ -4,7 +4,6 @@ module.exports = class Tournament1736916332376 {
     name = 'Tournament1736916332376'
 
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "tournamentRunners" DROP COLUMN "rate"`);
         await queryRunner.query(`ALTER TABLE "tournamentRunners" ADD "backRate" double precision NOT NULL DEFAULT '0'`);
         await queryRunner.query(`ALTER TABLE "tournamentRunners" ADD "layRate" double precision NOT NULL DEFAULT '0'`);
     }
@@ -12,6 +11,5 @@ module.exports = class Tournament1736916332376 {
     async down(queryRunner) {
         await queryRunner.query(`ALTER TABLE "tournamentRunners" DROP COLUMN "layRate"`);
         await queryRunner.query(`ALTER TABLE "tournamentRunners" DROP COLUMN "backRate"`);
-        await queryRunner.query(`ALTER TABLE "tournamentRunners" ADD "rate" double precision NOT NULL DEFAULT '0'`);
     }
 }
